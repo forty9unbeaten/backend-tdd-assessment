04363c33d6c3f0975e6551a8015b8f3efb40e415
Rob Spears <rob.sp3ars@gmail.com>
2020-03-03 15:10:38 -0500

    Write unittest to confirm help interface displays when no args are supplied

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -13,33 +13,44 @@ class TestEcho(unittest.TestCase):
         """ Test the help interface functionality """
         process = Popen(['python', 'echo.py', '-h'], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
-        stdout = stdout.decode().strip()
+        help_display = stdout.decode().strip()
         with open('USAGE', 'r') as f:
             help_output = f.read().strip()
-        self.assertEqual(stdout, help_output)
+        self.assertEqual(help_display, help_output)
 
     def test_upper(self):
         """ Test the --upper / -u functionality """
-        upper_out = check_output(
+        upper_output = check_output(
             ["python", "echo.py", "heLLo wOrld", "-u"]).decode().strip()
-        self.assertEqual(upper_out, 'HELLO WORLD')
+        self.assertEqual(upper_output, 'HELLO WORLD')
 
     def test_lower(self):
         """ Test the --lower / -u functionality """
-        lower_out = check_output(
+        lower_output = check_output(
             ["python", "echo.py", "heLLo wOrld", "-l"]).decode().strip()
-        self.assertEqual(lower_out, 'hello world')
+        self.assertEqual(lower_output, 'hello world')
 
     def test_title(self):
-        title_out = check_output(
+        """ Test the --title / -t functionality """
+        title_output = check_output(
             ["python", "echo.py", "heLLo wOrld", "-t"]).decode().strip()
-        self.assertEqual(title_out, 'Hello World')
+        self.assertEqual(title_output, 'Hello World')
 
     def test_all_args(self):
-        all_args_out = check_output(
+        """ Test functionality when all optional arguments are supplied """
+        all_args_output = check_output(
             ["python", "echo.py", "heLLo wOrld", "-ult"]).decode().strip()
         self.assertEqual(
-            all_args_out, 'Only one optional argument can be supplied.')
+            all_args_output, 'Only one optional argument can be supplied.')
+
+    def test_no_args(self):
+        """ Test help display when no arguments are supplied """
+        process = Popen(["python", "echo.py"], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        no_args_output = stdout.decode().strip()
+        with open('USAGE', 'r') as f:
+            help_output = f.read().strip()
+        self.assertEqual(no_args_output, help_output)
 
 
 if __name__ == '__main__':
